9e1a477214caaa67a5b98ae36b0ecefe9c0d1469
ahabib <ahabib@creativechaos.co>
2020-12-11 15:42:59 +0500

    Added sign up route for self sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { UserModel } from 'src/database/models/user.model';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth-guard';
 import { SkipAuth } from './skip-auth.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
 
   @SkipAuth()
   @UseGuards(LocalAuthGuard)
@@ -14,6 +26,11 @@ export class AuthController {
     return this.authService.login(req.user);
   }
 
+  @SkipAuth()
+  @Post('signup')
+  async create(@Body() props: Partial<UserModel>) {
+    return this.usersService.create(props);
+  }
   @Get('whoami')
   getProfile(@Request() req) {
     return req.user;
